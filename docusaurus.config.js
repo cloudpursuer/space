9613db902188243b3a7d3e86bb98649e37ab8773
cloudpursuer <cloudchaserxyz@gmail.com>
2023-01-04 14:38:04 +0800

    change the url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Cloudchaser',
   tagline: '前へススメ！',
-  url: 'https://vercel.com/cloudpursuer/space',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
